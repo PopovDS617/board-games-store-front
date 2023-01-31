3609dde0e952ea348748a8dd1555c18b9d09e04a
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2023-01-31 11:16:11 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,20 +29,19 @@ module.exports = {
       fuchia: colors.fuchia,
       pink: colors.pink,
       rose: colors.rose,
-
       black: colors.black,
       white: colors.white,
-      customGray: '#282c34',
-      customLightBlue: '#61dafb',
-      customBlue: '#40798c',
-      customTeal: '#70a9a1',
-      customDarkBlue: '#15272d',
-      customPaleGreen: '#9ec1a3',
-      customLightGreen: '#cfe0c3',
-      customYellow: '#FFD460',
-      customGreen: '#00dc82',
-      customPaleRed: '#d95757',
-      customRed: '#db0d0d',
+      customMilky:'#f4f1de',
+      customOrange:'#e07a5f',
+      customDarkBlue:'#3d405b',
+      customGreen:'#81b29a',
+      customYellow:'#f2cc8f'
+      
+      
+      
+      
+      
+      
     },
     fontFamily: {
      // segoeBold: ['segoeBold', 'sans-serif'],
